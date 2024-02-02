b9dcc447d73d84f60d067b590610f3dfadace28e
moaazbarbary <79059647+moaazbarbary@users.noreply.github.com>
2024-02-02 15:26:28 +0200

    Add files via upload

---
+++ new2024.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("hello world")
+st.image("https://theplace2b.com.au/wp-content/uploads/2020/09/178-1783296_g-transparent-circle-google-logo.png",width=200,caption="hi")
+st.header("moaaz barbary")
+st.subheader('Number of pickups by hour')
+st.radio("pick your gender",["male","female"])
+
+
